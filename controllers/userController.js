d451501d6ad825a2bb7673a0252c536e1b227044
JuanMa7u7 <jmlafuente97@gmail.com>
2025-04-04 21:55:53 -0600

    Actualizacion de usuarios, sanitizacion del objeto request

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import userModel from "../models/userModel.js";
+import sanitizeRequest from "../helpers/sanitizeRequest.js";
 import validateEmailFormat from "../helpers/validateEmailFormat.js";
 import validatePasswordSecurity from "../helpers/validatePasswordSecurity.js";
 
@@ -10,6 +11,7 @@ class UserController {
 
     async create(request, response) {
         try {
+            sanitizeRequest(request);
             const BODY = request.body;
             // SE VALIDA EL CUERPO DE LA PETICION
             if (typeof BODY.name == 'undefined')
@@ -43,15 +45,43 @@ class UserController {
 
     async update(request, response) {
         try {
-            response.status(201).json({ status: 'update-ok' });
+            sanitizeRequest(request);
+            const { id } = request.params;
+            const BODY = request.body || {};
+            // SE VALIDA EL CUERPO DE LA PETICION
+            if (typeof BODY.name != 'undefined')
+                if (typeof BODY.name != 'string')
+                    throw new Error('El campo nombre debe de ser una cadena de texto.');
+            if (typeof BODY.email != 'undefined') {
+                if (typeof BODY.email != 'string')
+                    throw new Error('El campo email debe de ser una cadena de texto.');
+                if (validateEmailFormat(BODY.email) == false)
+                    throw new Error('El email no tiene un formato correcto.');
+            }
+            if (typeof BODY.password != 'undefined') {
+                if (typeof BODY.password != 'string')
+                    throw new Error('El campo password debe de ser una cadena de texto.');
+                if (validatePasswordSecurity(BODY.password) == false)
+                    throw new Error('La contraseña no cumple con los requisitos de seguridad (mínimo 8 caracteres, mayúsculas, minúsculas y símbolos).');
+            }
+            if (typeof BODY.role != 'undefined') {
+                if (typeof BODY.role != 'string')
+                    throw new Error('El campo role debe de ser una cadena de texto.');
+            }
+
+            const data = await userModel.update(id, BODY);
+            if (data.success == true)
+                response.status(200).send('El usuario se actualizo de forma correcta.');
+            else
+                throw new Error('Error al actualizar el usuario.');
         } catch (e) {
-            response.status(500).send(e);
+            response.status(500).send(e.message);
         }
     }
 
     async delete(request, response) {
         try {
-            response.status(201).json({ status: 'delete-ok' });
+            response.status(206).json({ status: 'delete-ok' });
         } catch (e) {
             response.status(500).send(e);
         }
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import dbClient from "../config/dbClient.js"
 import getCurrentDate from "../helpers/getCurrentDate.js";
 
@@ -33,10 +34,51 @@ class UserModel {
         };
 
         const insertUser = await colUsers.insertOne(dataUser);
-        const insertedUserID = insertUser.insertedId.toString();
+        const insertedUserID = insertUser.insertedId;
 
         const colUserRoles = dbClient.db.collection('user_roles');
         await colUserRoles.insertOne({ user_id: insertedUserID, role_id: roleID });
+
+        return { success: true };
+    }
+
+    async update(id, data) {
+        const colUsers = dbClient.db.collection('users');
+        // SI SE ESPECIFICO UN NUEVO EMAIL, SE VERIFICA QUE NO ESTE UN USO
+        if (typeof data.email != 'undefined') {
+            const requestEmailIsAlreadyRegistered = await colUsers
+                .find({ "email": data.email, "_id": { "$ne": new ObjectId(id) } })
+                .project({ "email": true })
+                .limit(1)
+                .toArray();
+            if (requestEmailIsAlreadyRegistered.length > 0)
+                throw new Error('El email ya se encuentra registrado.');
+        }
+
+        let roleID;
+        if (typeof data.role != 'undefined') {
+            const colRoles = dbClient.db.collection('roles');
+            const requestRole = await colRoles
+                .find({ "name": data.role })
+                .project({ "_id": true })
+                .limit(1)
+                .toArray();
+            if (requestRole.length == 0)
+                throw new Error('El rol especificado no existe.');
+            roleID = requestRole[0]._id;
+        }
+
+        const dataUser = {
+            name: data.name || undefined,
+            email: data.email || undefined,
+            password: data.password || undefined
+        };
+
+        await colUsers.updateOne({ _id: new ObjectId(id) }, { "$set": dataUser });
+        if (roleID) {
+            const colUserRoles = dbClient.db.collection('user_roles');
+            await colUserRoles.updateOne({ user_id: new ObjectId(id) }, { "$set": { role_id: roleID } });
+        }
 
         return { success: true };
     }
